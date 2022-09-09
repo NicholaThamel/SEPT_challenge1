e38d1ce9b98c95c53a0dde25af35ee7b40b05159
NicholaThamel <s3863813@student.rmit.edu.au>
2022-09-09 22:26:15 +1000

    second commit

---
+++ src/main/java/com/example/dao/ItemsDao.java
@@ -0,0 +1,19 @@
+package com.example.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import org.springframework.stereotype.Component;
+
+import com.example.model.Item;
+
+@Component
+public class ItemsDao {
+
+    private static int i=0;
+    private static List<Item> items = new ArrayList<>();
+
+
+
+}
---
+++ src/main/java/com/example/model/Item.java
@@ -0,0 +1,22 @@
+package com.example.model;
+
+public class Item {
+
+    private int id;
+    private String name;
+    private String desc;
+    private double price;
+
+    public Item() {
+
+    }
+
+    public Item(int id, String name, String desc, double price) {
+        this.id = id;
+        this.name = name;
+        this.desc = desc;
+        this.price = price;
+    }
+
+
+}
